c587943be879611c3b8ef4357c79fd520a81ad5a
HIMURA Tomohiko <eiel.hal@gmail.com>
2024-06-17 00:27:07 +0900

    dev: time is string

--- src/index.mjs
+++ src/index.mjs
@@ -33,8 +33,8 @@ export function postDeploymentRequest() {
     api_key: env["INPUT_DATADOG-API-KEY"],
     service: env["INPUT_DATADOG-SERVICE-NAME"],
     env: env["INPUT_DATADOG-ENV"],
-    started_at: Number((env["INPUT_STARTED-AT"]) ?? 0) * 1e12,
-    finished_at: Number(env["INPUT_FINISHED-AT"]) * 1e12 || Date.now() * 1e6,
+    started_at: Number((env["INPUT_STARTED-AT"]) ?? 0) * 1e12.toString(),
+    finished_at: (Number(env["INPUT_FINISHED-AT"]) * 1e12 || Date.now() * 1e6).toString(),
     commit_sha: env["INPUT_GIT-COMMIT-SHA"] || env.GITHUB_SHA,
     repostiory_url: env["INPUT_GIT-REPOSITORY-URL"] || `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}`,
   };
