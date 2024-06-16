063f442e3508daef6067c1df232ad85fc425b63f
HIMURA Tomohiko <eiel.hal@gmail.com>
2024-06-16 23:46:54 +0900

    dev: fix typo

--- src/index.mjs
+++ src/index.mjs
@@ -28,16 +28,16 @@ function createRequest({ dd_host, api_key, service, env, started_at, finished_at
 }
 
 export function postDeploymentRequest() {
-    return createRequest({
-      dd_host: env.DD_SITE || 'datadoghq.com',
-      api_key: env["INPUT_DATADOG-API-KEY"],
-      service: env["INPUT_DATADOG-SERVICE-NAME"],
-      env: env["INPUT_DATADOG-ENV"],
-      started_at: Number(env[INPUT_started-at]) * 1e12,
-      finished_at: Number(env["INPUT_FINISHED-AT"]) * 1e12 ?? Date.now() * 1e6,
-      commit_sha: env["INPUT_GIT-COMMIT-SHA"] ?? env.GITHUB_SHA,
-      repostiory_url: env["INPUT_GIT-EPOSITORY-URL"] ?? `${env.GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}`,
-    });
+  return createRequest({
+    dd_host: env.DD_SITE || 'datadoghq.com',
+    api_key: env["INPUT_DATADOG-API-KEY"],
+    service: env["INPUT_DATADOG-SERVICE-NAME"],
+    env: env["INPUT_DATADOG-ENV"],
+    started_at: Number((env["INPUT_STARTED-AT"]) ?? 0) * 1e12,
+    finished_at: Number(env["INPUT_FINISHED-AT"]) * 1e12 ?? Date.now() * 1e6,
+    commit_sha: env["INPUT_GIT-COMMIT-SHA"] ?? env.GITHUB_SHA,
+    repostiory_url: env["INPUT_GIT-EPOSITORY-URL"] ?? `${env.GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}`,
+  });
 }
 
 function println(line) {
@@ -52,7 +52,6 @@ export async function run() {
   } catch(e) {
     println(e.toString());
   }
-  
 }
 
 await run();
